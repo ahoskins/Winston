854f8f0487223f021d2d0a8538591089dddd2051
Andrew Hoskins <andrew.hoskins16@gmail.com>
2014-10-05 21:14:31 -0600

    new ng app file structure

---
+++ angular_flask/scripts/factories/factory.js
@@ -0,0 +1,23 @@
+// Module
+//
+var demoIndex = angular.module('demoIndex', ['mm.foundation']);
+
+
+// Factory
+demoIndex.factory('courseFactory', function () {
+	var classes = [
+		{name: 'ECE 304',prof: 'Mani'},
+		{name: 'CMPUT 272',prof: 'Lorna'},
+		{name: 'CMPUT 274',prof: 'Nerd'},
+		{name: 'ECE 311',prof: 'Good man'},		
+		{name: 'ECE 340',prof: 'Woman?'}
+	];
+	
+	var factory ={};
+
+	factory.getClasses = function () {
+		return classes;
+	};
+
+	return factory;
+});
