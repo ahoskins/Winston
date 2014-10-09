71b8665e80d8e5be7d726740ee4c9d957108a384
Ross Anderson <ross.anderson@ualberta.ca>
2014-10-09 14:16:31 -0600

    Whitespace in runserver.py

--- runserver.py
+++ runserver.py
@@ -2,8 +2,8 @@ import os
 from angular_flask import app
 
 def runserver():
-	port = int(os.environ.get('PORT', 5000))
-	app.run(port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
 
 if __name__ == '__main__':
-	runserver()
+    runserver()
