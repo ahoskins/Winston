9467fef9bbe33608bc6e7accf9520736e8f652fe
Andrew Hoskins <andrew.hoskins16@gmail.com>
2014-10-08 20:54:24 -0600

    first successful API call from front-end

--- angular_flask/static/scripts/controllers/controller.js
+++ angular_flask/static/scripts/controllers/controller.js
@@ -1,12 +1,28 @@
 // Controllers
 //
 var controllers = {};
-controllers.CourseList = function ($scope, courseFactory) {
+
+controllers.CourseList = function ($scope, $window, courseFactory) {
 	$scope.classes = [];
 
+	// This will run on page load
+	// The return on the getClasses method is a "promise" (as they say in the JavaScripts)
+	// This "promise" will result in the success method being called if everything worker
+	// At this point, we de-serialize and bind this object to $scope.classes
+	//
 	function init() {
-		$scope.classes = courseFactory.getClasses();
+		courseFactory.getClasses().
+			success(function (data, status, headers, config) {
+				courseListing = angular.fromJson(data);
+				$scope.classes = courseListing.objects;
+			}) 
 	}
+
+	// $scope.doGreeting = function(greeting) {
+	// 	$window.alert();
+	// }
+
+	//$window.alert("test alert");
 
 	init();
 };
--- angular_flask/static/scripts/factories/factory.js
+++ angular_flask/static/scripts/factories/factory.js
@@ -4,19 +4,21 @@ var demoIndex = angular.module('demoIndex', ['mm.foundation']);
 
 
 // Factory
-demoIndex.factory('courseFactory', function () {
-	var classes = [
-		{name: 'ECE 304',prof: 'Mani'},
-		{name: 'CMPUT 272',prof: 'Lorna'},
-		{name: 'CMPUT 274',prof: 'Nerd'},
-		{name: 'ECE 311',prof: 'Good man'},		
-		{name: 'ECE 340',prof: 'Woman?'}
-	];
+demoIndex.factory('courseFactory', function ($window, $http, $q) {
+	// var classes = [
+	// 	{name: 'ECE 304',prof: 'Mani'},
+	// 	{name: 'CMPUT 272',prof: 'Lorna'},
+	// 	{name: 'CMPUT 274',prof: 'Nerd'},
+	// 	{name: 'ECE 311',prof: 'Good man'},		
+	// 	{name: 'ECE 340',prof: 'Woman?'}
+	// ];
 	
 	var factory ={};
 
 	factory.getClasses = function () {
-		return classes;
+		// Return a $http request and the promise associated with it
+		return( $http({method: 'GET', url: '/api/terms/1490/courses'}) );
+
 	};
 
 	return factory;
