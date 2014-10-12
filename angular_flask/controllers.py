b68d66eaaf55b34f59a1f04c612e6f3596e8c1e6
Ross Anderson <ross.anderson@ualberta.ca>
2014-10-11 22:49:56 -0600

    chore(Flask controller): route / to correct index.html, route 404 to 404.html
    Closes #3

--- angular_flask/controllers.py
+++ angular_flask/controllers.py
@@ -16,28 +16,7 @@ from angular_flask.core import db
 @app.route('/')
 @app.route('/index')
 def basic_pages(**kwargs):
-    return make_response(open('angular_flask/templates/index.html').read())
-
-# routing for CRUD-style endpoints
-# passes routing onto the angular frontend if the requested resource exists
-from sqlalchemy.sql import exists
-
-crud_url_models = app.config['CRUD_URL_MODELS']
-
-# @app.route('/<model_name>/')
-# @app.route('/<model_name>/<item_id>')
-# def rest_pages(model_name, item_id=None):
-#     if model_name in crud_url_models:
-#         print model_name
-#         print item_id
-#         model_class = crud_url_models[model_name]
-#         if item_id is None or session.query(exists().where(
-#             model_class.id == item_id)).scalar():
-#             print model_class
-#             print model_class.query.all()
-#             return make_response(open(
-#                 'views/index.html').read())
-#     abort(404)
+    return make_response(open('angular_flask/static/views/index.html').read())
 
 # special file handlers and error handlers
 # @app.route('/favicon.ico')
@@ -47,4 +26,4 @@ crud_url_models = app.config['CRUD_URL_MODELS']
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return make_response(open('angular_flask/static/views/index.html').read()), 404
+    return make_response(open('angular_flask/templates/404.html').read()), 404
