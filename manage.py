0ce789013af3a312b5b958bbf69c5e86d22b8a63
Ross Anderson <ross.anderson@ualberta.ca>
2014-10-13 12:37:03 -0600

    style(manage.py): move seed_db behaviour into a function

--- manage.py
+++ manage.py
@@ -15,6 +15,44 @@ def create_db():
 def drop_db():
     db.drop_all()
 
+def seed_db():
+    # Get the term list
+    terms = cal.get_term_list()
+    # Feed the term list into the database
+    for term in terms:
+        term_model = Term(term)
+        if not Term.query.get(term_model.term):
+            db.session.add(term_model)
+    try:
+        db.session.commit()
+    except:
+        print 'Terms failed to add to database'
+    else:
+        print 'All terms successfully added'
+
+    # Get the course list
+    if args.seedterm:
+        cal.select_current_term(args.seedterm)
+        courses = cal.get_courses_for_current_term()
+    else:
+        cal.select_current_term('1490')
+        courses = cal.get_courses_for_current_term()
+
+    # Feed the course list into the database
+    sys.stdout.write('Course 0/{}'.format(len(courses)))
+    sys.stdout.flush()
+    for course, i in zip(courses, range(len(courses))):
+        if not i % 100:
+            sys.stdout.write('\rCourse {}/{} added'.format(i, len(courses)))
+            sys.stdout.flush()
+        course_model = Course(course)
+        if not Course.query.get(course_model.course):
+            db.session.add(course_model)
+    try:
+        db.session.commit()
+    except:
+        print '\nCourses failed to add to database'
+
 def main():
     parser = argparse.ArgumentParser(description='Manage this Flask application.')
     parser.add_argument('command', help='the name of the command you want to run')
@@ -23,51 +61,15 @@ def main():
 
     if args.command == 'create_db':
         create_db()
-        print "DB created!"
+        print 'DB created!'
 
     elif args.command == 'delete_db':
         drop_db()
-        print "DB deleted!"
+        print 'DB deleted!'
 
     elif args.command == 'seed_db':
-        # Get the term list
-        terms = cal.get_term_list()
-        # Feed the term list into the database
-        for term in terms:
-            term_model = Term(term)
-            if not Term.query.get(term_model.term):
-                db.session.add(term_model)
-        try:
-            db.session.commit()
-        except:
-            print 'Terms failed to add to database'
-        else:
-            print 'All terms successfully added'
-
-        # Get the course list
-        if args.seedterm:
-            cal.select_current_term(args.seedterm)
-            courses = cal.get_courses_for_current_term()
-        else:
-            cal.select_current_term('1490')
-            courses = cal.get_courses_for_current_term()
-
-        # Feed the course list into the database
-        sys.stdout.write('Course 0/{}'.format(len(courses)))
-        sys.stdout.flush()
-        for course, i in zip(courses, range(len(courses))):
-            if not i % 100:
-                sys.stdout.write('\rCourse {}/{} added'.format(i, len(courses)))
-                sys.stdout.flush()
-            course_model = Course(course)
-            if not Course.query.get(course_model.course):
-                db.session.add(course_model)
-        try:
-            db.session.commit()
-        except:
-            print '\nCourses failed to add to database'
-        else:
-            print '\nAll courses successfully added'
+        seed_db()
+        print 'DB seeded!'
 
     else:
         raise Exception('Invalid command')
