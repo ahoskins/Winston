3218035b4cf21ce3e5a3ddffed153888c93db680
Ross Anderson <ross.anderson@ualberta.ca>
2014-10-15 10:19:09 -0600

    feat(api/courses-min): add concise course data endpoint for faster initial pageload
    Twenty seconds is a long time for a user to wait for pageload. There was too much data being sent, and very little of it was being read.
    Now, there is a new API endpoint which is faster and requires less pages.
    
    New API endpoint: /api/courses-min/
    Also, you should now specify the term you want courses in with a search query (example below).
    /api/courses-min/ loads 500 courses per page, with only 4 attributes per course (subject, subjectTitle, course, asString).
    Each request takes 168ms, and there are 8 pages.
    
    BREAKING CHANGE: Accessing a term's courses via /api/terms/<num>/courses is being deprecated. Instead, a [search query](http://flask-restless.readthedocs.org/en/latest/searchformat.html#quick-examples) should be used.
    e.g. OLD: GET /api/terms/1490/courses?page=<pagenum>
         NEW: GET /api/courses-min?q={"filters":[{"name":"term","op":"eq","val":1490}]}
    The linked docs have a good jQuery example: I'm sure it will be similar for the $http method you call.
    
    Is part of #9 (I'll close it if this is sufficient, otherwise I'll make the request smaller/faster)

--- angular_flask/api.py
+++ angular_flask/api.py
@@ -5,31 +5,24 @@ from angular_flask.models import *
 # API Routing
 # --------------------------------
 
-def post_terms_remove_courses(result=None, search_params=None, **kw):
-    """Accepts two arguments, `result`, which is the dictionary
-    representation of the JSON response which will be returned to the
-    client, and `search_params`, which is a dictionary containing the
-    search parameters for the request (that produced the specified
-    `result`).
-    """
-    if 'objects' in result:
-        for term in result['objects']:
-            term.pop('courses', None)
-
-def post_term_remove_course_sections(result=None, **kw):
-    if 'objects' in result:
-        term = result['objects']
-        for course in term:
-            course.pop('sections', None)
-
 # accessible at http://localhost:5000/api/terms
 api_manager.create_api(Term,
                        methods=['GET'],
-                       postprocessors={'GET_MANY': [post_terms_remove_courses],
-                                       'GET_SINGLE': [post_term_remove_course_sections]},
+                       exclude_columns=['courses', 'courses.sections'],
                        collection_name='terms')
 
 # accessible at http://localhost:5000/api/courses
 api_manager.create_api(Course,
                        methods=['GET'],
+                       exclude_columns=['sections'],
                        collection_name='courses')
+
+api_manager.create_api(Course,
+                       methods=['GET'],
+                       include_columns=['subject',
+                                        'subjectTitle',
+                                        'course',
+                                        'asString'],
+                       results_per_page=500,
+                       max_results_per_page=500,
+                       collection_name='courses-min')
