3a18befcf1d95bfff542b06911c444f898fd2f58
Andrew Hoskins <andrew.hoskins16@gmail.com>
2014-10-16 10:27:25 -0600

    refactor($scope): improve hard-coded getCourses, renamed various objects
    - The number of getCoursesPage requests is no longer hard-coded.  It will keep getting courses until the number of pages for that term.
    - Renamed all the many of the objects derived from subjectBin to make more logical and clear what that object contains.

--- angular_flask/static/scripts/controllers/controller.js
+++ angular_flask/static/scripts/controllers/controller.js
@@ -1,36 +1,53 @@
 // Classes Controller
 //
 coreModule.controller('CourseListCtrl', ['$scope', '$window', 'courseFactory', function($scope, $window, courseFactory) {
+	// New, organized course object
 	$scope.subjectBin = {};
 	var subjectBin = {};
+
+	// temp used in transfer to new course object
 	var pageListing;
-	for (var i = 0; i < 360; i++) {
-		courseFactory.getClasses(i).
-		success(function (data, status, headers, config) {
-			// Deserialize JSON data
-			pageListing = angular.fromJson(data);
 
-			// For each course on page of results...
-			pageListing.objects.forEach(function (course) {
-				// If the property is already there, just add to it
-				if (subjectBin.hasOwnProperty(course.subject)) {
-					// Push course object onto matching subjectBin property
-					subjectBin[course.subject].push(course);
-				}
-				else {
-					// If not there, create new property and add a first course to it
-					// Square brackets, [course] are necessary
-					subjectBin[course.subject] = [course];
-				}
-			})
+	// Counters
+	var page = 1;
+	var total_pages;
 
-		}).
-		error(function () {
-			$window.alert("Sorry, something when wrong.");
-			return -1;
-		});
-	}
+	// Purpose of first call is purely to scrape total_pages
+	courseFactory.getCoursesPage(1).
+	success(function (data, status, headers, config) {
+		pageListing = angular.fromJson(data);
+		total_pages = pageListing.total_pages;
 
+		while (page != (total_pages + 1)) {
+			courseFactory.getCoursesPage(page).
+			success(function (data, status, headers, config) {
+				// Deserialize JSON data
+				pageListing = angular.fromJson(data);
+
+				// For each course on page of results...
+				pageListing.objects.forEach(function (course) {
+					// If the property is already there, just add to it
+					if (subjectBin.hasOwnProperty(course.subject)) {
+						// Push course object onto matching subjectBin property
+						subjectBin[course.subject].push(course);
+					}
+					else {
+						// If not there, create new property and add a first course to it
+						// Square brackets, [course] are necessary
+						subjectBin[course.subject] = [course];
+					}
+				})
+			}).
+			error(function () {
+				$window.alert("Sorry, something when wrong.");
+			});	
+
+			// Increment to next page	
+			page = page + 1;	
+		}
+	})
+
+	// Gather new object
 	$scope.subjectBin = subjectBin;
 
 }]);
--- angular_flask/static/scripts/services/factory.js
+++ angular_flask/static/scripts/services/factory.js
@@ -3,7 +3,7 @@ coreModule.factory('courseFactory', function ($window, $http, $q) {
 	
 	var factory = {};
 
-	factory.getClasses = function (p) {
+	factory.getCoursesPage = function (p) {
 		// Return a $http request and the promise associated with it
 		return( $http({method: 'GET', url: '/api/terms/1490/courses?page=' + p }) );
 
