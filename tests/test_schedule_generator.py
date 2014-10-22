cfb4667b81f112033b856118c553a2ce0c291643
Ross Anderson <ross.anderson@ualberta.ca>
2014-10-22 14:42:22 -0600

    fix(Model IDs): replace schedule generation tests with String ids to match new tables

--- tests/test_schedule_generator.py
+++ tests/test_schedule_generator.py
@@ -3,13 +3,13 @@ from angular_flask.classtime.schedule_generator import ScheduleGenerator
 from angular_flask.classtime import cal
 
 def test_generate_schedule():
-    term = 1490
-    course_list = [2896, 1341]
+    term = '1490'
+    course_list = ['002896', '001341']
     generator = ScheduleGenerator(cal, term, course_list)
     schedules = generator.get_schedules()
     # assert schedules is None
 
-    term = 1490
-    course_list = [1343, 4093, 4096, 6768, 9019]
+    term = '1490'
+    course_list = ['001343', '004093', '004096', '006768', '009019']
     generator = ScheduleGenerator(cal, term, course_list)
     schedules = generator.get_schedules()
