4e4461f2d791af04aec7071fc287900c6c9ecd20
Ross Anderson <ross.anderson@ualberta.ca>
2014-11-01 14:21:32 -0600

    refactor(schedule-scoring): score schedules after section addition, not during comparison
    This should be a solid speed-up, especially in cases with large candidate pools.
    
    Closes #27

--- angular_flask/classtime/schedule.py
+++ angular_flask/classtime/schedule.py
@@ -28,8 +28,8 @@ class Schedule(object):
         elif not isinstance(sections, list):
             sections = [sections]
 
+        self.score = 0
         self.sections = []
-
         for section in sections:
             self.add_section(section)
 
@@ -51,22 +51,10 @@ class Schedule(object):
         elif len(self.sections) < len(other.sections):
             return Schedule.SELF_IS_WORSE
 
-        self_score, other_score = 0, 0
-
-        self_score += self.score_classes_in_a_row()
-        other_score += other.score_classes_in_a_row()
-
-        self_score += self.score_ideal_busy_range()
-        other_score += other.score_ideal_busy_range()
-
-        self_score += self.score_start_earlier()
-        other_score += other.score_start_earlier()
-
-        if self_score < other_score:
+        if self.score < other.score:
             return Schedule.SELF_IS_WORSE
         else:
             return Schedule.SELF_IS_BETTER
-
 
     def conflicts(self, section):
         """
@@ -100,15 +88,11 @@ class Schedule(object):
         for day in days:
             self._add_by_block(day, start, end, self.sections.index(section))
 
+        self.score_schedule()
         return self
 
     def clone(self):
         return Schedule(self.sections)
-
-    def add_section_with_deepishcopy(self, section):
-        ret = Schedule(self.sections)
-        ret.add_section(section)
-        return ret
 
     def _add_by_block(self, day, start, end, section_num):
         daynum = Schedule._daystr_to_daynum(day)
@@ -116,6 +100,68 @@ class Schedule(object):
             self.schedule_bitmap[daynum] += (1 << blocknum)
             self.schedule[daynum][blocknum] = section_num
 
+    # -------------------------------
+    # Schedule Evaluation Functions
+    #   used for sorting
+    # -------------------------------
+    def score_schedule(self):
+        self.score = 0
+        self.score += self._score_classes_in_a_row()
+        self.score += self._score_ideal_busy_range()
+        self.score += self._score_start_earlier()
+
+    def _score_classes_in_a_row(self):
+        """
+        A schedule is better if it has a more even distribution
+        of breaks throughout the day, and also if it squishes sections
+        together so as to not waste time. Constantly alternating between
+        class and break is bad. Having more than 3 hours in a row is bad.
+        """
+        max_consecutive_blocks = 3*2 # 3 hours x 2 blocks/hour
+        score = 0
+        schedule_bitmap_copy = list(self.schedule_bitmap)
+        for day in range(Schedule.NUM_DAYS):
+            consecutive_blocks = 0
+            day_bitmap = schedule_bitmap_copy[day]
+            while day_bitmap:
+                day_bitmap &= (day_bitmap << 1)
+                consecutive_blocks += 1
+            num_blocks_too_many = consecutive_blocks - max_consecutive_blocks
+            if num_blocks_too_many > 0:
+                score -= num_blocks_too_many
+        return score / 10.0
+
+    def _score_ideal_busy_range(self):
+        #               0 1 2 3 4 5 6 7 8 9 1011121 2 3 4 5 6 7 8 9 101112
+        bad_zone = int('11111111111111111100000000000000001111111111111111', 2)
+
+        for day in range(Schedule.NUM_DAYS):
+            in_bad_zone = self.schedule_bitmap[day] & bad_zone
+            num_outside = bin(in_bad_zone).count('1')
+        return -1*num_outside / 10.0
+
+    def _score_start_earlier(self):
+        score = 0
+
+        ideal_start_block = 8*2 # 8am
+        ideal_start_bitmap = (1 << ideal_start_block)
+        schedule_bitmap_copy = list(self.schedule_bitmap)
+        for day in range(Schedule.NUM_DAYS):
+            # Because a 1 further to the left (earlier in the day)
+            # will always make a larger number
+            if schedule_bitmap_copy[day] == 0:
+                continue
+            if schedule_bitmap_copy[day] > ideal_start_bitmap:
+                score += 2
+                continue
+            while schedule_bitmap_copy[day] < ideal_start_bitmap:
+                schedule_bitmap_copy[day] = schedule_bitmap_copy[day] << 1
+                score -= 1
+        return score
+
+    # ---------------------------
+    # Static Methods
+    # ---------------------------
     @staticmethod
     def _timestr_to_blocknum(time):
         if not isinstance(time, str):
@@ -137,57 +183,3 @@ class Schedule(object):
         if daystr not in Schedule.DAYS:
             raise ValueError('daystr must be in "{}"'.format(Schedule.DAYS))
         return Schedule.DAYS.index(daystr)
-
-    # -------------------------------
-    # Schedule Evaluation Functions
-    #   used for sorting
-    # All score_* methods should ideally return a value in the range [0,10]
-    # -------------------------------
-    def score_classes_in_a_row(self):
-        """
-        A schedule is better if it has a more even distribution
-        of breaks throughout the day, and also if it squishes sections
-        together so as to not waste time. Constantly alternating between
-        class and break is bad. Having more than 3 hours in a row is bad.
-        """
-        max_consecutive_blocks = 3*2 # 3 hours x 2 blocks/hour
-        score = 0
-        schedule_bitmap_copy = list(self.schedule_bitmap)
-        for day in range(Schedule.NUM_DAYS):
-            consecutive_blocks = 0
-            day_bitmap = schedule_bitmap_copy[day]
-            while day_bitmap:
-                day_bitmap &= (day_bitmap << 1)
-                consecutive_blocks += 1
-            num_blocks_too_many = consecutive_blocks - max_consecutive_blocks
-            if num_blocks_too_many > 0:
-                score -= num_blocks_too_many
-        return score / 10.0
-
-    def score_ideal_busy_range(self):
-        #               0 1 2 3 4 5 6 7 8 9 1011121 2 3 4 5 6 7 8 9 101112
-        bad_zone = int('11111111111111111100000000000000001111111111111111', 2)
-
-        for day in range(Schedule.NUM_DAYS):
-            in_bad_zone = self.schedule_bitmap[day] & bad_zone
-            num_outside = bin(in_bad_zone).count('1')
-        return -1*num_outside / 10.0
-
-    def score_start_earlier(self):
-        score = 0
-
-        ideal_start_block = 8*2 # 8am
-        ideal_start_bitmap = (1 << ideal_start_block)
-        schedule_bitmap_copy = list(self.schedule_bitmap)
-        for day in range(Schedule.NUM_DAYS):
-            # Because a 1 further to the left (earlier in the day)
-            # will always make a larger number
-            if schedule_bitmap_copy[day] == 0:
-                continue
-            if schedule_bitmap_copy[day] > ideal_start_bitmap:
-                score += 2
-                continue
-            while schedule_bitmap_copy[day] < ideal_start_bitmap:
-                schedule_bitmap_copy[day] = schedule_bitmap_copy[day] << 1
-                score -= 1
-        return score
