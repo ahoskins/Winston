f0f10e633d4248d2c328a8bd8e1835c87ebe7f4b
Ross Anderson <ross.anderson@ualberta.ca>
2014-11-05 19:03:53 -0700

    docs(remote_db): docstrings

--- angular_flask/classtime/remote_db/abstract_remotedb.py
+++ angular_flask/classtime/remote_db/abstract_remotedb.py
@@ -4,59 +4,45 @@ class AbstractRemoteDatabase(object):
     inherit from.
 
     Defines methods which all connections should implement.
-
-    AcademicCalendar should be initialized with an instance
-    of a class which implements these methods - that is,
-    AcademicCalendar should not need to know what type
-    of database it is getting course information from.
     """
 
     def connect(self, **kwargs):
-        """
-        Connect to the course info database
-        To be called during initialization
+        """Connect to the course info database
         """
         raise NotImplementedError()
 
     def disconnect(self, **kwargs):
-        """
-        Disconnect from the course info database.
-        To be called on cleanup
+        """Disconnect from the course info database
         """
         raise NotImplementedError()
 
     def save_search(self, name, **kwargs):
-        """
-        Save a search by name, such that it may be used many times
-        later. Also improves semantics.
+        """Save a search by name
 
-        kwargs holds the parameters for the search
-          eg for LDAP servers, kwargs holds search_flt, attrs, limit, and path
+        :param kwargs: parameters for the search.
+            eg for LDAP servers, kwargs holds:
+            * search_flt
+            * attrs
+            * limit
+            * path
 
-        These should be:
-        1) Defined in a config file,
-        2) Loaded into the database object at runtime
+        Typical usage: parameters are defined in a config file,
+        and ``save_search`` is used to save them for later usage
         """
         raise NotImplementedError()
 
     def search(self, name, **kwargs):
-        """
-        Search the database by a saved search name. This will look different for
-        different institutions and database types, so use kwargs to define
-        parameters to the search
+        """Search the database using a saved search
 
-        Can also take kwargs as a method of passing search-time variable
-        parameters.
-
-        Is a convenience wrapper for _search()
+        Can also take kwargs as a method of passing options
+        which change at runtime
         """
         raise NotImplementedError()
 
     def _search(self, **kwargs):
-        """
-        Internal search method - this actually *does* the searching.
+        """Internal search method - this actually *does* the searching.
 
-        search(name) should call this method with the proper parameters
-        as the specific database connection calls for.
+        :py:func:`search` should call this method, passing
+        in the proper kwargs
         """
         raise NotImplementedError()
