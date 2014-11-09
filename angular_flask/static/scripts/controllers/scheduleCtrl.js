ac57eae5e184f1e08f8de1239743ccd6c0d301b2
Andrew Hoskins <andrew.hoskins16@gmail.com>
2014-11-09 14:06:46 -0700

    feat(schedule): correct time format, improved calendar view

--- angular_flask/static/scripts/controllers/scheduleCtrl.js
+++ angular_flask/static/scripts/controllers/scheduleCtrl.js
@@ -4,27 +4,31 @@
 coreModule.controller('scheduleCtrl', ['$scope', '$window', '$rootScope', function($scope, $window, $rootScope) {
     // Event array
     $scope.events = [
-        {title: 'All Day Event',start: new Date('Tue Nov 5 2014 09:00:00 GMT+0530 (IST)'),end: new Date('Tue Nov 5 2014 10:00:00 GMT+0530 (IST)')},
-        {title: 'Long Event',start: new Date('Tue Nov 5 2014 10:00:00 GMT+0530 (IST)'),end: new Date('Tue Nov 5 2014 11:00:00 GMT+0530 (IST)')},
-        {id: 999,title: 'Repeating Event',start: new Date('Tue Nov 4 2014 09:00:00 GMT+0530 (IST)'),allDay: false}
+        {title: 'All Day Event',start: new Date('Tue Nov 11 2014 09:00:00 MDT'),end: new Date('Tue Nov 11 2014 10:00:00 MDT')}
     ];
 
     // EventSources (required as binding to angular HTML directive)
     $scope.eventSources = [$scope.events];
 
     // General calendar config
+    // Nov 10 -14 is displaying...this is the current week
     $scope.uiConfig = {
-        calendar:{
-            height: 450,
-            editable: true,
-            header:{
-                left: 'title',
-                center: '',
-                right: 'today prev,next'
-            },
+        calendar: {
+            height: 1000,
+            //editable: true,
+            header: false,
+
+            // Format
             defaultView: 'agendaWeek',
             columnFormat: "dddd",
-            hiddenDays: [0,6]
+            hiddenDays: [0,6],
+            minTime: '08:00:00',
+            maxTime: '21:00:00',
+            allDaySlot: false,
+
+            // Event settings
+            allDayDefault: false
+            //timezoneParam: 'local'
         }
     };
 
