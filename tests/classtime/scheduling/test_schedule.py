3469c8f68c3ee56b32cfef1930bf830acf8cd0bb
Ross Anderson <ross.anderson@ualberta.ca>
2014-11-09 17:03:34 -0700

    tests(schedule): rewrite Schedule tests
    Related to #39

--- tests/classtime/scheduling/test_schedule.py
+++ tests/classtime/scheduling/test_schedule.py
@@ -3,162 +3,214 @@ import unittest
 
 from classtime.scheduling import Schedule
 
-class TestSchedule(unittest.TestCase):
+class TestSchedule(unittest.TestCase): #pylint: disable=R0904
     @classmethod
-    
-def check_add_section(section, numblocks_expected):
-    """
-    Check that adding a given section to a new Schedule
-    - fills the expected number of schedule blocks, and
-    - fills the correct schedule blocks
-    """
-    schedule = Schedule(section)
-    numblocks = 0
-    for day in schedule.timetable:
-        for block in day:
-            if block is not Schedule.OPEN:
-                numblocks += 1
-    assert numblocks == numblocks_expected
+    def setup_class(cls):
+        pass
 
-def test_add_section():
-    """
-    Test section dictionary addition to a Schedule
-    object, including proper conversion of startTime
-    and endTime strings to their proper schedule blocks
-    """
-    sections = [
-        {
-            'day': 'TR',
-            'startTime': '08:00 AM',
-            'endTime': '08:50 AM',
+    @classmethod
+    def teardown_class(cls):
+        pass
 
-            'expectedBusyBlocks': 4
-        },
-        {
-            'day': 'MTWRF',
-            'startTime': '08:00 AM',
-            'endTime': '08:50 AM',
+    def test_section_add(self):
+        testcases = [
+            {
+                'day': 'TR',
+                'startTime': '08:00 AM',
+                'endTime': '08:50 AM',
 
-            'expectedBusyBlocks': 10
-        },
-        {
-            'day': 'TR',
-            'startTime': '08:00 AM',
-            'endTime': '08:50 PM',
+                'expected': 4
+            },
+            {
+                'day': 'MTWRF',
+                'startTime': '08:00 AM',
+                'endTime': '08:50 AM',
 
-            'expectedBusyBlocks': 52
-        },
-        {
-            'day': 'TR',
-            'startTime': '08:00 AM',
-            'endTime': '09:20 AM',
+                'expected': 10
+            },
+            {
+                'day': 'TR',
+                'startTime': '08:00 AM',
+                'endTime': '08:50 PM',
 
-            'expectedBusyBlocks': 6
-        },
-        {
-            'day': 'M',
-            'startTime': '06:00 PM',
-            'endTime': '08:50 PM',
+                'expected': 52
+            },
+            {
+                'day': 'TR',
+                'startTime': '08:00 AM',
+                'endTime': '09:20 AM',
 
-            'expectedBusyBlocks': 6
-        }
-    ]
-    for section in sections:
-        yield check_add_section, section, section['expectedBusyBlocks']
+                'expected': 6
+            },
+            {
+                'day': 'M',
+                'startTime': '06:00 PM',
+                'endTime': '08:50 PM',
 
-def check_conflict(sections, has_conflict):
-    """
-    Assert that a list of sections has either:
-    - one or more conflicts, or
-    - no conflicts
-    """
-    timetable = Schedule()
-    for section in sections:
-        if timetable.conflicts(section):
-            assert has_conflict == True
-            return
-        else:
-            timetable.add_section(section)
-    assert has_conflict == False
+                'expected': 6
+            }
+        ]
+        for section in testcases:
+            self.assert_section_add(section,
+                section.get('expected'))
 
-def test_conflicts():
-    """
-    Test conflict detection between a schedule and
-    a candidate section.
-    """
-    scenarios = [
-        {
-            'expected': True,
-            'sections':
-            [
-                {
-                    'day': 'TR',
-                    'startTime': '08:00 AM',
-                    'endTime': '08:50 AM'
-                },
-                {
-                    'day': 'MTWRF',
-                    'startTime': '08:00 AM',
-                    'endTime': '08:50 AM'
-                }
-            ]
-        },
-        {
-            'expected': False,
-            'sections':
-            [
-                {
-                    'day': 'TR',
-                    'startTime': '08:00 AM',
-                    'endTime': '08:50 AM'
-                },
-                {
-                    'day': 'TR',
-                    'startTime': '09:00 AM',
-                    'endTime': '09:50 AM'
-                }
-            ]
-        },
-        {
-            'expected': False,
-            'sections':
-            [
-                {
-                    'day': 'TR',
-                    'startTime': '07:00 PM',
-                    'endTime': '07:50 PM'
-                },
-                {
-                    'day': 'TR',
-                    'startTime': '08:00 PM',
-                    'endTime': '09:00 PM'
-                }
-            ]
-        },
-        {
-            'expected': True,
-            'sections':
-            [
-                {
-                    'day': 'TR',
-                    'startTime': '07:00 PM',
-                    'endTime': '07:50 PM'
-                },
-                {
-                    'day': 'TR',
-                    'startTime': '08:00 PM',
-                    'endTime': '09:00 PM'
-                },
-                {
-                    'day': 'TR',
-                    'startTime': '08:30 PM',
-                    'endTime': '08:50 PM'
-                }
-            ]
-        }
-    ]
-    
-    for scenario in scenarios:
-        sections = scenario.get('sections')
-        expected = scenario.get('expected')
-        yield check_conflict, sections, expected
+    def assert_section_add(self, section, numblocks_expected): #pylint: disable=R0201
+        """
+        Check that adding a given section to a new Schedule
+        - fills the expected number of timetable blocks
+        """
+        schedule = Schedule()
+        schedule.add_section(section)
+        numblocks = 0
+        for day in schedule.timetable:
+            for block in day:
+                if block is not Schedule.OPEN:
+                    numblocks += 1
+        assert numblocks == numblocks_expected
+
+    def test_busy_time_add(self):
+        testcases = [
+            {
+                'day': 'TR',
+                'startTime': '08:00 AM',
+                'endTime': '08:50 AM',
+
+                'expected': 4
+            },
+            {
+                'day': 'MTWRF',
+                'startTime': '08:00 AM',
+                'endTime': '08:50 AM',
+
+                'expected': 10
+            },
+            {
+                'day': 'TR',
+                'startTime': '08:00 AM',
+                'endTime': '08:50 PM',
+
+                'expected': 52
+            },
+            {
+                'day': 'TR',
+                'startTime': '08:00 AM',
+                'endTime': '09:20 AM',
+
+                'expected': 6
+            },
+            {
+                'day': 'M',
+                'startTime': '06:00 PM',
+                'endTime': '08:50 PM',
+
+                'expected': 6
+            }
+        ]
+        for section in testcases:
+            self.assert_busy_time_add(section,
+                section.get('expected'))
+
+    def assert_busy_time_add(self, busy_time, numblocks_expected): #pylint: disable=R0201
+        """
+        Check that adding a given busy_time to a new Schedule
+        - fills the expected number of timetable blocks
+        """
+        schedule = Schedule()
+        schedule.add_busy_time(busy_time)
+        numblocks = 0
+        for day in schedule.timetable:
+            for block in day:
+                if block is not Schedule.OPEN:
+                    numblocks += 1
+        assert numblocks == numblocks_expected
+
+    def test_conflict_recognition(self): #pylint: disable=R0201
+        testcases = [
+            {
+                'expected': True,
+                'sections':
+                [
+                    {
+                        'day': 'TR',
+                        'startTime': '08:00 AM',
+                        'endTime': '08:50 AM'
+                    },
+                    {
+                        'day': 'MTWRF',
+                        'startTime': '08:00 AM',
+                        'endTime': '08:50 AM'
+                    }
+                ]
+            },
+            {
+                'expected': False,
+                'sections':
+                [
+                    {
+                        'day': 'TR',
+                        'startTime': '08:00 AM',
+                        'endTime': '08:50 AM'
+                    },
+                    {
+                        'day': 'TR',
+                        'startTime': '09:00 AM',
+                        'endTime': '09:50 AM'
+                    }
+                ]
+            },
+            {
+                'expected': False,
+                'sections':
+                [
+                    {
+                        'day': 'TR',
+                        'startTime': '07:00 PM',
+                        'endTime': '07:50 PM'
+                    },
+                    {
+                        'day': 'TR',
+                        'startTime': '08:00 PM',
+                        'endTime': '09:00 PM'
+                    }
+                ]
+            },
+            {
+                'expected': True,
+                'sections':
+                [
+                    {
+                        'day': 'TR',
+                        'startTime': '07:00 PM',
+                        'endTime': '07:50 PM'
+                    },
+                    {
+                        'day': 'TR',
+                        'startTime': '08:00 PM',
+                        'endTime': '09:00 PM'
+                    },
+                    {
+                        'day': 'TR',
+                        'startTime': '08:30 PM',
+                        'endTime': '08:50 PM'
+                    }
+                ]
+            }
+        ]
+        for scenario in testcases:
+            self.assert_conflict_recognition(scenario.get('sections'),
+                scenario.get('expected'))
+
+    def assert_conflict_recognition(self, sections, has_conflict): #pylint: disable=R0201
+        """
+        Assert that a list of sections has either:
+        - one or more conflicts, or
+        - no conflicts
+        """
+        schedule = Schedule()
+        for section in sections:
+            if schedule.conflicts(section):
+                assert has_conflict == True
+                return
+            else:
+                schedule.add_section(section)
+        assert has_conflict == False
