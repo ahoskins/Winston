18f0d024a24a1252934a4536a87e6f29c845ad1e
Ross Anderson <ross.anderson@ualberta.ca>
2014-11-13 14:23:57 -0700

    chore(runserver): is_main_process instead of 'not is_reloader_process'

--- runserver.py
+++ runserver.py
@@ -1,11 +1,11 @@
 import os
 from angular_flask import app
 
-def is_reloader_process():
-    return not os.environ.get('WERKZEUG_RUN_MAIN')
+def is_main_process():
+    return os.environ.get('WERKZEUG_RUN_MAIN')
 
 def runserver():
-    if not is_reloader_process():
+    if is_main_process():
         from classtime import AcademicCalendar
         for institution in ['ualberta']:
             AcademicCalendar.idly_fill(institution)
