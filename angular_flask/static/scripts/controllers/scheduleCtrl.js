6cb6d8b309bb7e216435067dbd58c5fd47b76c9d
Andrew Hoskins <andrew.hoskins16@gmail.com>
2014-11-12 21:15:10 -0700

    feat(generate-schedules): scroll through all generated schedules

--- angular_flask/static/scripts/controllers/scheduleCtrl.js
+++ angular_flask/static/scripts/controllers/scheduleCtrl.js
@@ -30,115 +30,165 @@ coreModule.controller('scheduleCtrl', ['$scope', '$window', '$rootScope', 'sched
         }
     };
 
+
     var renderSchedule = function (scheduleListing) {
 
-        scheduleListing.objects[0].sections.forEach(function (classtime) {
+        scheduleLength = scheduleListing.objects.length;
 
-            // Time //
-            //
-            //
-            var startTimeString = classtime.startTime.match(/(\d+):(\d+)/),
-                endTimeString = classtime.endTime.match(/(\d+):(\d+)/);
+        // Return closure of scheduleListing
+        //
+        // @param {int}: schedule index within the JSON response
+        //
+        // @return {void}: updates $scope.events
+        return function (i) {
 
-            // Minute
-            var startMinute = parseInt(startTimeString[2]),
-                endMinute = parseInt(endTimeString[2]);
+            scheduleListing.objects[i].sections.forEach(function (classtime) {
 
-            // Hour
-            var startHour;
-            if (classtime.startTime.match(/PM/) && startTimeString[1] != 12) {
-                // PM
-                startHour = parseInt(startTimeString[1]) + 12;
-            }
-            else {
-                // AM
-                startHour = parseInt(startTimeString[1]);
-            }
+                // Time //
+                //
+                //
+                var startTimeString = classtime.startTime.match(/(\d+):(\d+)/),
+                    endTimeString = classtime.endTime.match(/(\d+):(\d+)/);
 
-            var endHour;
-            if (classtime.endTime.match(/PM/) && endTimeString[1] != 12) {
-                // PM
-                endHour = parseInt(endTimeString[1]) + 12;
-            }
-            else {
-                // AM
-                endHour = parseInt(endTimeString[1]);
-            }
+                // Minute
+                var startMinute = parseInt(startTimeString[2]),
+                    endMinute = parseInt(endTimeString[2]);
 
-            // Day //
-            //
-            //
-            var date = new Date(),
-                d = date.getDate(),
-                m = date.getMonth(),
-                y = date.getFullYear();
+                // Hour
+                var startHour;
+                if (classtime.startTime.match(/PM/) && startTimeString[1] != 12) {
+                    // PM
+                    startHour = parseInt(startTimeString[1]) + 12;
+                }
+                else {
+                    // AM
+                    startHour = parseInt(startTimeString[1]);
+                }
 
-            // JavaScript function Date.getDay()
-            //
-            // Sunday 0
-            // Monday 1
-            // Tuesday 2
-            // Wednesday 3
-            // Thursday 4
-            // Friday 5
-            // Saturday 6
-            //
-            // @return {int} enumeration of current day of the week
-            var dayNumber = date.getDay(),
-                offset;
+                var endHour;
+                if (classtime.endTime.match(/PM/) && endTimeString[1] != 12) {
+                    // PM
+                    endHour = parseInt(endTimeString[1]) + 12;
+                }
+                else {
+                    // AM
+                    endHour = parseInt(endTimeString[1]);
+                }
+
+                // Day //
+                //
+                //
+                var date = new Date(),
+                    d = date.getDate(),
+                    m = date.getMonth(),
+                    y = date.getFullYear();
+
+                // JavaScript function Date.getDay()
+                //
+                // Sunday 0
+                // Monday 1
+                // Tuesday 2
+                // Wednesday 3
+                // Thursday 4
+                // Friday 5
+                // Saturday 6
+                //
+                // @return {int} enumeration of current day of the week
+                var dayNumber = date.getDay(),
+                    offset;
 
 
-            // Use the current day {int:0:6} of the week
-            // Enumerate each day of the week {int:0:6}
-            // and find the offset {int:0:6}
-            // Use this offset to find calendar day number  {int:0:31}
-            if (classtime.day.match(/M/)) {
-                offset = 1 - dayNumber;
-                addEvent(offset);
-            }
+                // Use the current day {int:0:6} of the week
+                // Enumerate each day of the week {int:0:6}
+                // and find the offset {int:0:6}
+                // Use this offset to find calendar day number  {int:0:31}
+                if (classtime.day.match(/M/)) {
+                    offset = 1 - dayNumber;
+                    addEvent(offset);
+                }
 
-            if (classtime.day.match(/T/)) {
-                offset = 2 - dayNumber;
-                addEvent(offset);
-            }
+                if (classtime.day.match(/T/)) {
+                    offset = 2 - dayNumber;
+                    addEvent(offset);
+                }
 
-            if (classtime.day.match(/W/)) {
-                offset = 3 - dayNumber;
-                addEvent(offset);
-            }
+                if (classtime.day.match(/W/)) {
+                    offset = 3 - dayNumber;
+                    addEvent(offset);
+                }
 
-            if (classtime.day.match(/R/)) {
-                offset = 4 - dayNumber;
-                addEvent(offset);
-            }
+                if (classtime.day.match(/R/)) {
+                    offset = 4 - dayNumber;
+                    addEvent(offset);
+                }
 
-            if (classtime.day.match(/F/)) {
-                offset = 5 - dayNumber;
-                addEvent(offset);
-            }
+                if (classtime.day.match(/F/)) {
+                    offset = 5 - dayNumber;
+                    addEvent(offset);
+                }
 
-            // Add event using parameters:
-            // asString
-            // Time
-            // Day
-            //
-            // @returns {void} an event is added to $scope.events array
-            function addEvent(offset) {
-                $scope.events.push({
-                    title: classtime.asString,
-                    start: new Date(y,m,d + offset, startHour, startMinute),
-                    end: new Date(y,m,d + offset,endHour,endMinute)
-                });
-            }
-        });
+                // Add event using parameters:
+                // asString
+                // Time
+                // Day
+                //
+                // @returns {void} an event is added to $scope.events array
+                function addEvent(offset) {
+                    $scope.events.push({
+                        title: classtime.asString,
+                        start: new Date(y,m,d + offset, startHour, startMinute),
+                        end: new Date(y,m,d + offset,endHour,endMinute)
+                    });
+                }
+            });
+        };
+
     };
 
+
+    var scheduleInstance,
+        scheduleLength,
+        scheduleIndex = 0;
+
+    // Event handle for prev/next buttons
+    //
+    $scope.displayDifferentSchedule = function (forward) {
+
+        // Adjust schedule index
+        if (forward) {
+            scheduleIndex = scheduleIndex + 1;
+            if (scheduleIndex >= scheduleLength) {
+                scheduleIndex = scheduleLength - 1;
+            }
+        }
+        else {
+            scheduleIndex = scheduleIndex - 1;
+            if (scheduleIndex < 0) {
+                scheduleIndex = 0;
+            }
+        }
+
+        // Clear current events
+        while ($scope.events.length > 0) {
+            $scope.events.pop();
+        }
+
+        // Invoke the closure on the new index
+        scheduleInstance(scheduleIndex);
+    };
+
+    // Event handle for gen-sched button
+    //
     $scope.getSchedules = function () {
         scheduleFactory.getSchedules($rootScope.addedCourses).
         success(function (data) {
                 var scheduleListing = angular.fromJson(data);
 
-                renderSchedule(scheduleListing);
+                // Create closure with current scheduleListing
+                scheduleInstance = renderSchedule(scheduleListing);
+
+                // Invoke the closure
+                scheduleInstance(0);
         }).
         error(function() {
                $window.alert("error dude");
