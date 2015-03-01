8320eb1e7b1cadf49deb8a452b2f65c54eecf10c
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-02-28 22:36:49 -0700

    drawable busy times editor added

--- public/js/controllers/scheduleCtrl.js
+++ public/js/controllers/scheduleCtrl.js
@@ -5,6 +5,28 @@ Includes Full Calendar config, prev/next buttons, and add more courses button
 */
 winstonControllers.controller('scheduleCtrl', ['$scope', '$window', '$location', 'uiCalendarConfig', '$timeout', 'SubjectBin', 'readyMadeSchedules', function($scope, $window, $location, uiCalendarConfig, $timeout, SubjectBin, readyMadeSchedules) {
 
+    // click each calendar events causes dialog box with course info
+
+    /*
+    ******************************************************
+    Get the schedule data from readyMadeSchedules service
+    ******************************************************
+    */
+
+    // Array of ready to go schedules in Full Calendar format
+    var arrayOfSchedules = readyMadeSchedules.getReadyMadeSchedules();
+
+    $scope.scheduleLength = arrayOfSchedules.length;
+    $scope.scheduleIndex = 0;
+
+    $scope.events = arrayOfSchedules[0];
+
+    // For some reason, uiCalendarConfig is not populated right when the page loads
+    $timeout(function() {
+        uiCalendarConfig.calendars.weekView.fullCalendar('removeEvents');
+        uiCalendarConfig.calendars.weekView.fullCalendar('addEventSource', $scope.events);
+    }, 500);
+
     /* 
     ***************************************
     Full Calendar Config
@@ -14,9 +36,46 @@ winstonControllers.controller('scheduleCtrl', ['$scope', '$window', '$location',
     // Note: It is displaying the current week (with the day number hidden)
     $scope.uiConfig = {
         calendar: {
+            // Use theme and then $UI themeroller if I want
+            // editable: true,
+
+            // buttons on events of probably this: http://fullcalendar.io/docs/views/Custom_Views/
+
             height: 1000,
-            //editable: true,
             header: false,
+
+            selectable: true,
+            // selectHelper: true,
+            select: function(start, end) {
+                $scope.$apply(function(){
+
+                    $scope.events.push({
+                        durationEditable: true,
+                        title: 'Busy Time',
+                        start: start,
+                        end: end,
+                        color: '#EF9A9A'
+                    });
+
+                    uiCalendarConfig.calendars.weekView.fullCalendar('removeEvents');
+                    uiCalendarConfig.calendars.weekView.fullCalendar('addEventSource', $scope.events);
+                });
+            },
+            eventClick: function(calEvent, jsEvent, view) {
+                // Only allowed to delete busy times
+                if (calEvent.title !== 'Busy Time') {
+                    return;
+                }
+
+                // Delete the event
+                var index = $scope.events.indexOf(calEvent);
+                $scope.$apply(function() {
+                    $scope.events.splice(index, 1);
+
+                    uiCalendarConfig.calendars.weekView.fullCalendar('removeEvents');
+                    uiCalendarConfig.calendars.weekView.fullCalendar('addEventSource', $scope.events);
+                });
+            },
 
             defaultView: 'agendaWeek',
             columnFormat: "dddd",
@@ -32,27 +91,9 @@ winstonControllers.controller('scheduleCtrl', ['$scope', '$window', '$location',
     };
 
     /*
-    ******************************************************
-    Get the schedule data from readyMadeSchedules service
-    ******************************************************
-    */
-
-    // Array of ready to go schedules in Full Calendar format
-    var arrayOfSchedules = readyMadeSchedules.getReadyMadeSchedules();
-
-    var schedules = JSON.stringify(arrayOfSchedules);
-    console.log(schedules);
-
-    $scope.scheduleLength = arrayOfSchedules.length;
-    $scope.scheduleIndex = 0;
-
-    // eventSources is a single, complete schedule
-    $scope.eventSources = [arrayOfSchedules[0]];
-
-    /*
-    ***********************
-    Handle clicks on buttons
-    ***********************
+    ****************************************
+    Handle clicks on toggle schedule button
+    ****************************************
     */
 
     // Event handle for prev/next buttons
@@ -82,10 +123,8 @@ winstonControllers.controller('scheduleCtrl', ['$scope', '$window', '$location',
         uiCalendarConfig.calendars.weekView.fullCalendar('addEventSource', $scope.events);
     };
 
-    // // Event hanlde for add more courses button
-    // $scope.showAccordion = function () {
-    //     // switch to accordion view
-    //     $location.path('/find-courses');
-    // }
+    $scope.backToBrowse = function() {
+        $location.path('/browse');
+    }
 
 }]);
