d6ccd4f65f4affae2db1e95b79fc6756f1795ca3
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-03-08 14:18:36 -0600

    preferences-sliders: ui component added with flexbox layout

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // Module
 //
-var winstonApp = angular.module('winstonApp', ['winstonControllers', 'ui.calendar', 'pmkr.filterStabilize', 'ui.bootstrap', 'ngRoute', 'ngFacebook', 'ngProgressLite']);
+var winstonApp = angular.module('winstonApp', ['winstonControllers', 'ui.calendar', 'pmkr.filterStabilize', 'ui.bootstrap', 'ngRoute', 'ngFacebook', 'ngProgressLite', 'ngAnimate', 'ngMaterial']);
 
 var winstonControllers = angular.module('winstonControllers', ['ui.calendar', 'pmkr.filterStabilize']);
 
