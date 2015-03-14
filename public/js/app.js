b7f93f6275ad166a674d8a40c15c6bbc0e854f83
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-03-13 22:47:36 -0600

    facebook social share button, remove twitter meta because not working

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 // Module
 //
-var winstonApp = angular.module('winstonApp', ['winstonControllers', 'LocalStorageModule', 'ui.calendar', 'pmkr.filterStabilize', 'ui.bootstrap', 'ngRoute', 'ngFacebook', 'ngProgressLite', 'ngAnimate', 'ngMaterial']);
+var winstonApp = angular.module('winstonApp', ['winstonControllers', 'LocalStorageModule', 'ui.calendar', 'pmkr.filterStabilize', 'ui.bootstrap', 'ngRoute', 'ngFacebook', 'ngProgressLite', 'ngAnimate', 'ngMaterial', 'djds4rce.angular-socialshare']);
 
 var winstonControllers = angular.module('winstonControllers', ['ui.calendar']);
 
