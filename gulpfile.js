afaf4cf590e5fa9f361c0b0cc96bc3237d7edb89
Andrew Hoskins <andrew.hoskins16@gmail.com>
2015-03-21 23:36:15 -0600

    gulp: bower libs and other libs and concatenated and minified in prod

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,24 @@ var concat = require('gulp-concat');
 var notify = require('gulp-notify');
 var htmlreplace = require('gulp-html-replace');
 var minifiy_css = require('gulp-minify-css');
+var main_bower = require('main-bower-files');
+var filter = require('gulp-filter');
 
 var bower_dir = 'public/js/lib/vendor/bower_components';
 
+/**
+Starting point for gulp... the default task is a blank task depending on the array passed in
+*/
+gulp.task('default', ['minify-js', 'replace-js', 'minify-css', 'replace-css', 'minify-bower', 'replace-bower', 'minify-libs', 'replace-libs']);
 
-gulp.task('default', ['minify-js', 'replace-js', 'minify-css', 'replace-css']);
 
+/****************************************
+                 Minify
+*****************************************/
+
+/**
+These are all js files from winstonApp
+*/
 gulp.task('minify-js', function() {
   return gulp.src(['public/js/app.js', 'public/js/controllers/*.js', 'public/js/services/*.js'])
   	.pipe(concat('app.min.js'))
@@ -19,14 +31,17 @@ gulp.task('minify-js', function() {
   	.pipe(notify({ message: 'Finished minifying app (excluding bower)'}));
 });
 
+/**
+Make sure to manually add new css libs here
+*/
 gulp.task('minify-css', function() {
-  return gulp.src(['public/js/lib/vendor/bower_components/normalize.css/normalize.css', 
-    'public/js/lib/vendor/bower_components/bootstrap/dist/css/bootstrap.css', 
-    'public/js/lib/vendor/bower_components/angular-material/angular-material.css', 
-    'public/js/lib/vendor/bower_components/fullcalendar/dist/fullcalendar.css', 
-    'public/js/lib/vendor/bower_components/font-awesome/css/font-awesome.css', 
-    'public/js/lib/vendor/bower_components/ngprogress-lite/ngprogress-lite.css', 
-    'public/js/lib/vendor/bower_components/angular-socialshare/angular-socialshare.css', 
+  return gulp.src([bower_dir + '/normalize.css/normalize.css', 
+    bower_dir + '/bootstrap/dist/css/bootstrap.css', 
+    bower_dir + '/angular-material/angular-material.css', 
+    bower_dir + '/fullcalendar/dist/fullcalendar.css', 
+    bower_dir + '/font-awesome/css/font-awesome.css', 
+    bower_dir + '/ngprogress-lite/ngprogress-lite.css', 
+    bower_dir + '/angular-socialshare/angular-socialshare.css', 
     'public/css/style.css'])
   .pipe(concat('css.min.css'))
   .pipe(minifiy_css())
@@ -34,13 +49,65 @@ gulp.task('minify-css', function() {
   .pipe(notify({ message: 'Finished minifying css'}));
 });
 
+/**
+This grabs all main bower js files...if its in bower, its included in here
+*/
+gulp.task('minify-bower', function() {
+  var jsFilter = filter('*.js')
+  return gulp.src(main_bower())
+    .pipe(jsFilter)
+    .pipe(concat('bower.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/js/bower/bower-min'))
+    .pipe(notify({ message: 'Finished minifying bower'}))
+    .pipe(jsFilter.restore());
+});
+
+/**
+Make sure to manually add new libs into this!
+*/
+gulp.task('minify-libs', function() {
+  return gulp.src(['public/js/lib/calendar.js',
+    'public/js/lib/filterStabilize.js',
+    'public/js/lib/fuse.min.js',
+    'public/js/lib/memoize.js',
+    'public/js/lib/twitter.min.js'
+    ])
+  .pipe(concat('libs.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('public/js/libs/libs-min'))
+  .pipe(notify({ message: 'Finished minifying libs'}));
+});
+
+/****************************************
+                  Replace
+*****************************************/
+
+gulp.task('replace-libs', ['minify-libs'], function() {
+  return gulp.src('views/libs.html')
+  .pipe(htmlreplace({
+    'js': 'js/libs/libs-min/libs.min.js'
+  }))
+  .pipe(notify({ message: 'Finished replacing libs in html'}))
+  .pipe(gulp.dest('views/build'));
+})
+
+gulp.task('replace-bower', ['minify-bower'], function() {
+  gulp.src('views/bower.html')
+  .pipe(htmlreplace({
+    'js': 'js/bower/bower-min/bower.min.js'
+  }))
+  .pipe(notify({ message: 'Finished replacing bower in html'}))
+  .pipe(gulp.dest('views/build'));
+});
+
 gulp.task('replace-js', ['minify-js'], function() {
 	gulp.src('views/includes.html')
 	.pipe(htmlreplace({
   		'js': 'js/app-min/app.min.js'
   	}))
-  	.pipe(notify({ message: 'Finished replacing js in html'}))
-  	.pipe(gulp.dest('views/build'));
+  .pipe(notify({ message: 'Finished replacing js in html'}))
+  .pipe(gulp.dest('views/build'));
 });
 
 gulp.task('replace-css', ['minify-css'], function() {
@@ -51,3 +118,4 @@ gulp.task('replace-css', ['minify-css'], function() {
   .pipe(notify({ message: 'Finished replacing css in html'}))
   .pipe(gulp.dest('views/build'));
 });
+
